d2d75e8f52cb4d97388acf2bc32632ea5366f3fa
Leooman <yonghenglion@gmail.com>
2018-07-05 18:17:04 +0800

    默认布局

--- src/page/home.vue
+++ src/page/home.vue
@@ -1,5 +1,19 @@
 <template>
-  <userInfo :userInfo="userInfo"></userInfo>
+  <el-container class="full_container">
+    <el-header>
+      <h3 @click="toggleAside">Vadmin Default Layout</h3>
+    </el-header>
+    <el-container class="main_container">
+      <el-aside :style="asideStyle">
+        <el-menu router>
+          <el-menu-item v-for="item in menu" :index=item.path><i class="el-icon-menu"></i>{{item.title}}</el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <userInfo :userInfo="userInfo"></userInfo>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -10,21 +24,58 @@ export default {
   },
   data () {
     return {
-      
+      menu:[],
+      collapse:false
     }
   },
   computed: {
     ...mapState({
       userInfo:state => state.userInfo
-    })
+    }),
+    asideStyle () {
+      return {
+        width: `${this.collapse ? '60' : '200'}px`
+      }
+    }
+  },
+  mounted(){
+    let menu=new Array(20).fill({
+        title:"导航",
+        path:"home"
+      })
+    this.menu.push(...menu)
   },
   methods: {
-    
+    toggleAside(){
+      this.collapse=!this.collapse
+    }
   }
 }
 </script>
 
 <style lang="less" scoped>
+  .full_container{
+    height: 100%;
+    .main_container{
+      height: calc(~"(100% - 60px)");
+    }
+  }
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
   
+  .el-aside {
+    .el-menu{
+      min-height: 100%;
+    }
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    position: relative;
+  }
 </style>
 
--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ const routes = [
 	{
 		path: '/home',
 		component: home,
-		meta:{auth:true}
+		// meta:{auth:true}
 	},
 ]
 
